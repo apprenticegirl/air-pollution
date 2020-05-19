13f0380edbadb1e35e9c2de2644c8a89dc47c8cf
apprenticegirl <laura.bendayan@gmail.com>
2020-05-19 14:18:06 +0200

    Update contaminacion.R

--- contaminacion.R
+++ contaminacion.R
@@ -5,7 +5,10 @@ require(reshape)
 require(reshape2)
 require(stringr)
 
-#modificamos la tabla para que cada fila sea un registro
+#primero lo primero, hay que cargar el dataset
+contaminacion <- read.csv(file = 'air pollution.csv')
+
+#modificamos el dataset para que cada columna de día se convierta en una fila
 melt.contaminacion <- melt(contaminacion, id = c("PROVINCIA","MUNICIPIO", "ESTACION","MAGNITUD","PUNTO_MUESTREO","ANO","MES"))
 
 #creamos uno para los dias y otro para la señal de válido
@@ -23,24 +26,15 @@ merge.contaminacion<-merge(v.contaminacion,d.contaminacion, by=c("PROVINCIA", "M
 merge.contaminacion$FECHA<-as.Date(with(merge.contaminacion, paste(ANO, MES, id,sep="-")), "%Y-%m-%d")
 merge.contaminacion$CIUDAD<-'Madrid'
 
-#lo ultimo sería eliminar los valores=N
-merge.contaminacion <- merge.contaminacion[ which(merge.contaminacion$value.x=='V'), ]
-#UNICAMENTE SON VÁLIDOS LOS DATOS QUE LLEVAN EL CÓDIGO DE VALIDACIÓN “V".
+#lo ultimo sería eliminar los valores no válidos y dar toques en el formato
+merge.contaminacion <- merge.contaminacion[ which(merge.contaminacion$value.x=='V'), ] #UNICAMENTE SON VÁLIDOS LOS DATOS QUE LLEVAN EL CÓDIGO DE VALIDACIÓN “V".
 merge.contaminacion$VALOR<-merge.contaminacion$value.y
 merge.contaminacion$MAGNITUD<-as.character(merge.contaminacion$MAGNITUD)
 
+#nos quedamos con las variables y observaciones que nos interesan
 contaminacion.df<-merge.contaminacion %>%
   select('MAGNITUD','PUNTO_MUESTREO','VALOR','FECHA','CIUDAD') %>%
   filter(MAGNITUD %in% c('1','8','9','10','14'))
 
-
+#imprimimos el dataset 
 write.csv(contaminacion.df,file="contaminacion madrid dia.csv", row.names = F)
-
-
-#reemplazamos estacion por la verdadera estacion
-#merge.contaminacion$ESTACION<-as.character(sub('_.*', '', merge.contaminacion$PUNTO_MUESTREO))
-
-
-
-
-
